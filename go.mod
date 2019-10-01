d37d8094c01a7923e755bb575dd774ceb5207939
masa-suzu <11026517+masa-suzu@users.noreply.github.com>
2019-10-01 23:41:40 +0900

    1.3.1 Chat with clients

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.13
 
 require (
 	github.com/golangci/golangci-lint v1.19.1 // indirect
+	github.com/gorilla/websocket v1.4.1
 	golang.org/x/tools v0.0.0-20190929041059-e7abfedfabcf // indirect
 )
--- main.go
+++ main.go
@@ -14,16 +14,18 @@ type templateHandler struct {
 	template *template.Template
 }
 
-func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	t.once.Do(func() {
 		t.template = template.Must(template.ParseFiles(filepath.Join("templates", t.fileName)))
 	})
-
-	_ = t.template.Execute(w, nil)
+	_ = t.template.Execute(w, req)
 }
 
 func main() {
-	http.Handle("/", &templateHandler{fileName: "template.html"})
+	r := newRoom()
+	http.Handle("/", &templateHandler{fileName: "chat.html"})
+	http.Handle("/room", r)
+	go r.run()
 
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServe", err)
